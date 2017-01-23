900d10d4d13144661377c413f1672b4d163b92ca
zitongliu <zitongliu7@gmail.com>
2017-01-23 14:38:36 +1100

    Controller/view work in progress - chatrooms

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,2 +1,13 @@
 class ChatroomsController < ApplicationController
+  def index
+    @chatrooms = Chatroom.all
+
+  end
+  def show
+    @chatroom = Chatroom.find_by( :id => params[:id] )
+    @messages = Message.all
+    @message = Message.new
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :chatrooms, only: [:new, :create, :show, :index]
+  root to: 'pages#home'
 
-root to: 'pages#home'
+
 
 end
