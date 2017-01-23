1e70459ff2e1f2b5c4dddb08e88eb8d83732e159
zitongliu <zitongliu7@gmail.com>
2017-01-23 15:36:37 +1100

    Added seeds

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,4 +1,4 @@
 class Chatroom < ApplicationRecord
   belongs_to :admin, :class_name => User, :foreign_key => :user_id
-  has_and_belongs_to_many :users # Think of these as members
+  has_and_belongs_to_many :users, optional: true # Think of these as members
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "USERS!"
+User.destroy_all
+
+u1 = User.create :username => "groucho", :name => 'Groucho Marx', :email => 'groucho@ga.co', :password => "chicken", :password_confirmation => "chicken"
+
+u2 = User.create :username => "harpo", :name => 'Harpo Marx', :email => 'kristina@email.com', :password => "chicken", :password_confirmation => "chicken"
+
+u3 = User.create :username => "zeppo", :name => 'Zeppo Marx', :email => 'josephs@email.com', :password => "chicken", :password_confirmation => "chicken"
+
+
+puts "\tUser count: #{ User.all.count }"
+
+
+puts "CHATROOMS!"
+Chatroom.destroy_all
+
+c1 = Chatroom.create :name => "Random", :user_id => u1.id
+c1.users << u2 << u3
+
+c2 = Chatroom.create :name => "General", :user_id => u2.id
+c2.users << u2 << u3
+
+puts "\tChatroom count: #{ Chatroom.all.count }"
