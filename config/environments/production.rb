0779a79eefebca5346afed73c612cb5761aca382
zitongliu <zitongliu7@gmail.com>
2017-02-04 13:09:16 +1100

    Responsive test 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
   config.public_file_server.enabled = true
 
   config.serve_static_assets = true
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
